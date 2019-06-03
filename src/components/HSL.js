5abfe4866ff8a6a582fc65b90411f3c275d70ce1
Allan Hall <bigal76126@gmail.com>
2019-06-03 14:11:55 -0400

    added the 4 sliders using hooks

--- src/components/HSL.js
+++ src/components/HSL.js
@@ -2,12 +2,70 @@ import React, { Component, useState } from 'react'
 
 export default function HSL() {
   //set state variables here
-
+  const [hue, setHue] = useState(180)
+  const [saturation, setSaturation] = useState(50)
+  const [lightness, setLightness] = useState(50)
+  const [alpha, setAlpha] = useState(0.5)
   // create functions for events here
 
   return (
     <>
-      <h1>Color Picker 2.0!</h1>
+      <section className="pattern">
+        <main
+          className="sliders"
+          style={{
+            backgroundColor: `hsl(${hue}, ${saturation}%, ${lightness}%, ${alpha})`
+          }}
+        >
+          <header className="title">
+            Pick a color, any color but Goldenrod
+          </header>
+          <div>
+            <input
+              type="range"
+              min="1"
+              max="360"
+              class="slider"
+              step="1"
+              value={hue}
+              onChange={e => setHue(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="range"
+              min="1"
+              max="100"
+              class="slider"
+              step="1"
+              value={saturation}
+              onChange={e => setSaturation(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="range"
+              min="1"
+              max="100"
+              class="slider"
+              step="1"
+              value={lightness}
+              onChange={e => setLightness(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="range"
+              min="0"
+              max="1"
+              class="slider"
+              step="0.01"
+              value={alpha}
+              onChange={e => setAlpha(e.target.value)}
+            />
+          </div>
+        </main>
+      </section>
     </>
   )
 }
